444238d240516af70f56bdbb107bc1f9c3691812
Mokammel Hossain Tanvir <mokammeltanvir01@gmail.com>
2023-05-02 22:52:32 +0600

    Tweets and show frontend

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\TweetSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,12 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
         $this->call(TweetSeeder::class);
     }
 }
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,24 +1,31 @@
 <script setup>
-import { Head, Link } from '@inertiajs/vue3';
-import TwitterLayout from '@/Layouts/TwitterLayout.vue';
+    import { ref } from 'vue';
+    import { Head, Link } from '@inertiajs/vue3';
+    import Tweet from '@/Components/Tweet.vue'
+    import TwitterLayout from '@/Layouts/TwitterLayout.vue';
+
+    defineProps({ tweets: Array })
 
 </script>
 
 <template>
+
     <Head title="Twitter" />
+
     <TwitterLayout>
-    <div class="text-white">
+        <div class="text-white">
 
             <div class="flex" v-for="tweet in tweets" :key="tweet">
                 <Tweet :tweet="tweet"/>
             </div>
             <div class="border-b border-b-gray-800 mt-2"></div>
-            </div>
-    </TwitterLayout>
+        </div>
 
+    </TwitterLayout>
 </template>
+
 <style>
-    body {
-        background-color: black;
-    }
+body {
+    background-color: black;
+}
 </style>
