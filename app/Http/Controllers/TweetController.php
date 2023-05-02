418e3549330a1e2cc1255ea082c0aad1d19726cd
Mokammel Hossain Tanvir <mokammeltanvir01@gmail.com>
2023-05-02 22:39:03 +0600

    routes create add tweet

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -4,62 +4,78 @@ namespace App\Http\Controllers;
 
 use App\Models\Tweet;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class TweetController extends Controller
 {
     /**
      * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+        return Inertia::render('Welcome', [
+            'tweets' => Tweet::orderBy('id', 'desc')->get()
+        ]);
     }
 
     /**
      * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
-    }
+        $file = null;
+        $extension = null;
+        $fileName = null;
+        $path = '';
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Tweet $tweet)
-    {
-        //
-    }
+        if ($request->hasFile('file')) {
+            $file = $request->file('file');
+            $request->validate([ 'file' => 'required|mimes:jpg,jpeg,png,mp4' ]);
+            $extension = $file->getClientOriginalExtension();
+            $fileName = time() . '.' . $extension;
+            $extension === 'mp4' ? $path = '/videos/' : $path = '/pics/';
+        }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Tweet $tweet)
-    {
-        //
-    }
+        $tweet = new Tweet;
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Tweet $tweet)
-    {
-        //
+        $tweet->name = 'Mokammel Hossain Tanvir';
+        $tweet->handle = '@mokammeltanvir';
+        $tweet->image = 'https://pbs.twimg.com/profile_images/1458760157945491460/mT93gmJh_400x400.jpg';
+        $tweet->tweet = $request->input('tweet');
+        if ($fileName) {
+            $tweet->file = $path . $fileName;
+            $tweet->is_video = $extension === 'mp4' ? true : false;
+            $file->move(public_path() . $path, $fileName);
+        }
+        $tweet->comments = rand(5, 500);
+        $tweet->retweets = rand(5, 500);
+        $tweet->likes = rand(5, 500);
+        $tweet->analytics = rand(5, 500);
+
+        $tweet->save();
     }
 
     /**
      * Remove the specified resource from storage.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
      */
-    public function destroy(Tweet $tweet)
+    public function destroy($id)
     {
-        //
+        $tweet = Tweet::find($id);
+
+        if (!is_null($tweet->file) && file_exists(public_path() . $tweet->file)) {
+            unlink(public_path() . $tweet->file);
+        }
+
+        $tweet->delete();
+
+        return redirect()->route('tweets.index');
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,6 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        $this->call(TweetsSeeder::class);
+        $this->call(TweetSeeder::class);
     }
 }
