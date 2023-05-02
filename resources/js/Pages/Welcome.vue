ccb9cb9d1b322129e027efe5280a4b13075085a5
Mokammel Hossain Tanvir <mokammeltanvir01@gmail.com>
2023-05-02 21:54:12 +0600

    Tweet

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -5,15 +5,15 @@ import TwitterLayout from '@/Layouts/TwitterLayout.vue';
 </script>
 
 <template>
-    <Head title="Welcome" />
+    <Head title="Twitter" />
     <TwitterLayout>
-     <div class="text-white">
+    <div class="text-white">
 
-            <div class="flex" >
-                
+            <div class="flex" v-for="tweet in tweets" :key="tweet">
+                <Tweet :tweet="tweet"/>
             </div>
             <div class="border-b border-b-gray-800 mt-2"></div>
-        </div>
+            </div>
     </TwitterLayout>
 
 </template>
